9ce51330cd4d43d1e3ee9d9b36a1f8bc569e7410
Müge Ersöz <Muge@Muge-Ersozs-MacBook-Air.local>
2017-01-19 19:49:59 +0200

    Added images and functions to game

--- main.js
+++ main.js
@@ -1,12 +1,52 @@
-console.log("JS file is connected to HTML! Woo!")
+console.log("JS file is connected to HTML! Woo!");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
 
-if ( cardTwo === cardOne ) {
-	alert("You found a match!");
-} else {
-	alert("Sorry, try again.");
-}
+/* Function for appending cards to game board*/
+var dealCards = function(){
+	for ( var i = 0; i < cards.length; i++){
+		var cardElement = document.createElement("div");
+		cardElement.className = "card";
+		cardElement.setAttribute('data-card', cards[i]);
+
+	    cardElement.addEventListener('click', isTwoCards);
+		document.getElementById("game-board").appendChild(cardElement);
+	};
+};
+
+/* Deal cards when game is started */
+var clickToStart = document.getElementById("startGame");
+clickToStart.addEventListener( 'click', dealCards );
+
+var cardsInPlay = [];
+var cardElement = " ";
+
+var isTwoCards = function(){	
+	cardClicked = this.getAttribute('data-card');
+	cardsInPlay.push(cardClicked);
+
+	/* Add image of card */
+	var image = document.createElement("img");
+	if ( this.getAttribute('data-card') === "king"){
+		image.src = "spades_king.png";
+		image.alt = 'King of Spades';
+	} else if (	this.getAttribute('data-card') === "queen"){
+		image.src = "clubs_queen.png";
+		image.alt = 'Queen of Clubs';
+	};
+	image.style.cssText = 'height:200px; width: 150px;';
+	this.appendChild(image);
+
+	if (cardsInPlay.length === 2 ) {
+    	if( isMatch(cardsInPlay) ){
+	    	console.log("it's a match!");
+	    };
+    	cardsInPlay = [];
+	};
+};
+
+var isMatch = function(cardsInPlay){
+	if (cardsInPlay[0] === cardsInPlay[1]){
+		return true;
+	};
+};
